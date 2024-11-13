53dd9f7a4b103e664510a0ba5909a5b7e7298f43
romas@null.net <Peder4566695!>
2024-11-13 09:38:46 -0500

    romas: good and ai images

--- image_service.py
+++ image_service.py
@@ -1,6 +1,10 @@
+import exifread
 from fastapi import File, UploadFile, HTTPException, FastAPI
 import requests, os, json
 import uvicorn
+from PIL import Image
+from io import BytesIO, StringIO
+
 
 app = FastAPI()
 
@@ -12,10 +16,24 @@ params = {
 
 @app.post("/test/")
 async def create_upload_file(file: UploadFile):
+    image_tags = {}
     data = await file.read()
+
+    tags = {}
+    tags = exifread.process_file(BytesIO(data))
+
+    for tag in tags:
+        if tag.startswith('JPEGThumbnail'):
+            continue
+#        print("{tag}: {value}".format(tag=tag, value=tags.get(tag)))
+        image_tags[tag] = tags.get(tag)
+    print(image_tags)
     files = { 'media': data }
     r = requests.post('https://api.sightengine.com/1.0/check.json', files=files, data=params)
-    return json.loads(r.text)
+    return {
+        'ai_screening': json.loads(r.text),
+        'exif_tags': image_tags
+    }
 
 
 uvicorn.run(app)
